7d2f2662ccf0f91e645a0b6da51413cc20cbf6a6
ryotaro oda <plm23.ccc@gmail.com>
2020-04-10 23:59:29 +0900

    fix: routes fmt

--- internal/server/routes/route.go
+++ internal/server/routes/route.go
@@ -31,7 +31,7 @@ func recoverAndServe(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
-				log.Error(fmt.Sprintf("%w", err))
+				log.Error(fmt.Sprintf("%v", err))
 				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 			}
 		}()
